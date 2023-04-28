fe17cdb32a889b66b90dc88365809f077c909510
Arnav Sharma <20bma016@nith.ac.in>
2023-04-28 23:41:05 +0530

    static dirs updated

--- PriceAPI/PriceAPI/settings.py
+++ PriceAPI/PriceAPI/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
-("js", os.path.join(STATIC_ROOT, 'js')),
-("css", os.path.join(STATIC_ROOT, 'css')),
-("images", os.path.join(STATIC_ROOT, 'images')),
-("fonts", os.path.join(STATIC_ROOT, 'fonts')),
+("js", os.path.join(STATIC_ROOT, 'rest_framework/js')),
+("css", os.path.join(STATIC_ROOT, 'rest_framework/css')),
+("img", os.path.join(STATIC_ROOT, 'rest_framework/img')),
+("fonts", os.path.join(STATIC_ROOT, 'rest_framework/fonts')),
+("docs", os.path.join(STATIC_ROOT, 'rest_framework/docs')),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
